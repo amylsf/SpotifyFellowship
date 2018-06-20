6e511238c48013aa74c25cde2f1fd1be2a8ba2da
Amy San Felipe <amysanfelipe@gmail.com>
2018-06-20 15:16:55 -0400

    Complete Question 1 - sortByStrings

---
+++ Question 1.js
@@ -0,0 +1,16 @@
+var sortByStrings = function(s, t) {
+  let output = '';
+  let letters = {};
+
+  for (let i  = 0; i < s.length; i++) {
+    letters[s[i]] ? letters[s[i]]++ : letters[s[i]] = 1;
+  }
+
+  for (let j = 0; j < t.length; j++) {
+    if (letters[t[j]]) {
+      output += t[j].repeat(letters[t[j]]);
+    }
+  }
+
+  return output;
+}
